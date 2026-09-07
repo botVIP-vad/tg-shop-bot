e0b4881bc9228232524ce8a65bfd6ce844a46734
botVIP-vad <avdadavas2@gmail.com>
2026-09-07 15:40:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def main():
     dp.include_router(user.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot) 
 
 
 if __name__ == "__main__":
