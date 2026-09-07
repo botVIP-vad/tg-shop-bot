19fa583720737fc9cb6c7cf20e7d1d4ee97e63a9
botVIP-vad <avdadavas2@gmail.com>
2026-09-07 16:01:51 +0200

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -74,7 +74,7 @@ async def render_screen(screen_name: str, state: FSMContext, bot: Bot, chat_id: 
             bot.send_message(
                 chat_id,
                 "Привет! 👋/n/n"
-                "Это бот для заказа сайтов. Нажми «🌐 Заказать сайт», чтобы посмотреть доступные предложения."
+                "БОТ ВРЕМЕННО НЕ РАБОТАЕТ ЧТОБЫ ЗАКАЗАТЬ САЙТ ИЛИ БОТА ПИСАТЬ - @vadymtm90"
                 + status_line,
                 reply_markup=main_menu_kb(is_admin(user_id)),
             ),
